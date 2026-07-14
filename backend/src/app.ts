27179a35762f2d1e5dfc84e5c1d369b49fd36678
shreshth-xd <shreshthkanchan777@gmail.com>
2026-07-14 23:46:32 +0530

    Set up the webhook router to route the webhook related requests/pings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import healthRouter from "./routes/health.routes";
 import expensesRouter from "./routes/expenses.routes";
-
+import webhookRouter from "./routes/webhook.routes";
 
 const app = express();
 
 app.use("/expenses", expensesRouter);
+app.use("/webhook", express.raw({ type: "application/json" }), webhookRouter);
 app.use(express.json());
 
 app.use("/health", healthRouter);
